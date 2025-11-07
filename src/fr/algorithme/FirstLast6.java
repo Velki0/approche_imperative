a92375eb32a712829b4abc6056469b72c67fb3ef
Velki0 <b.gerardin125@gmail.com>
2025-11-07 17:51:16 +0100

    Correction FirstLast6

--- src/fr/algorithme/FirstLast6.java
+++ src/fr/algorithme/FirstLast6.java
@@ -4,10 +4,10 @@ public class FirstLast6 {
 
     public static void main(String[] args) {
 
-        int[] tableau = {6, 6, 3} ;
+        int[] tableau = {} ;
 
         // 6 au début ou à la fin du tableau ?
-        if (tableau != null && tableau.length > 0) {
+        if (tableau.length > 0) {
             boolean is6atStartEnd = (tableau[0] == 6) || (tableau[tableau.length - 1] == 6) ? true : false;
             System.out.println(is6atStartEnd);
         } else  {
