feccb362eda3fbb2b9500c3b4fa7ca0775523a2a
Velki0 <b.gerardin125@gmail.com>
2025-11-07 12:25:36 +0100

    Version initial, TP03 et TP04

---
+++ src/fr/operateurs/DeclarationApp.java
@@ -0,0 +1,21 @@
+package fr.operateurs;
+
+public class DeclarationApp {
+    public static void main(String[] args) {
+        byte a = (byte) 127;
+        short b = (short) 127;
+        int c = (int) 127;
+        long d = (long) 127;
+        float e = (float) 127;
+        double g = (double) 127;
+        char f = (char) 127;
+        String h = "Hello World!";
+
+        System.out.println(h);
+
+        String randomString = "Voici le résultat d’un calcul : 1+5=6";
+        System.out.println(randomString);
+        randomString = "Voici le résultat d’un calcul :\n1+5=6";
+        System.out.println(randomString);
+    }
+}
